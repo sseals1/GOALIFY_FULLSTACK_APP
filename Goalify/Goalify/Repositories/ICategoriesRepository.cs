e26592873eb78e9138582ebb19ce888458020830
Sutherland Seals <sseals1@gmail.com>
2023-01-30 11:51:49 -0600

    Added the GetById Repo method to get a single Category

---
+++ Goalify/Goalify/Repositories/ICategoriesRepository.cs
@@ -0,0 +1,11 @@
+﻿using Goalify.Models;
+using System.Collections.Generic;
+
+namespace Goalify.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        Categories Get(int id);
+        List<Categories> GetAll();
+    }
+}
