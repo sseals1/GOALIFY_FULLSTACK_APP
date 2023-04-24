3b197466f41e19e0b82feb8aae7e1e85277fa4e8
Sutherland Seals <sseals1@gmail.com>
2023-04-24 13:25:34 -0600

    Changed CSS background color on Tips dropdown menu to white

--- Goalify/Goalify/client/src/Goal_Keeper_App/src/components/views/suggestions/Tips.js
+++ Goalify/Goalify/client/src/Goal_Keeper_App/src/components/views/suggestions/Tips.js
@@ -1,37 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { getTips } from "../../ApiManager";
 import { useParams } from "react-router-dom";
-import { Container, Row, Col} from "react-bootstrap"
-import "./Tips.css"
+import { Container, Row, Col } from "react-bootstrap";
+import "./Tips.css";
 
 export const Tips = () => {
   const [tips, setTips] = useState({});
   const { tipsId } = useParams();
 
   useEffect(() => {
-    getTips(tipsId)
-    .then((data) => {
+    getTips(tipsId).then((data) => {
       setTips(data);
-      console.log(data)
+      console.log(data);
     });
-  },[]);
+  }, []);
 
   return (
-
     <Container>
       <Row>
         <h4 className="h4">{tips.suggestion}</h4>
       </Row>
       <div className="tips">
-      <Row>
-        <Col xs={5}>{tips.tip}</Col>
-      </Row>
+        <Row>
+          <Col xs={8}>{tips.tip}</Col>
+        </Row>
         <label htmlFor="name"></label>
-       
-         
-        
       </div>
-      </Container>
-
+    </Container>
   );
 };
